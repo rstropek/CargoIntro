e04345dfe765912b01b5b13844f42d2182e029b3
Rainer Stropek <rainer@software-architects.at>
2020-07-30 09:54:00 +0200

    Cleanup sample

--- samples/11-crates-deps-lib/mth_calc/src/lib.rs
+++ samples/11-crates-deps-lib/mth_calc/src/lib.rs
@@ -1,40 +1,5 @@
 use std::thread;
 
-/// Runs a given closure in parallel on all available CPUs.
-/// 
-/// The given closure can return a value. All return values of all closure executions are 
-/// aggregated with a given aggregation closure. The method returns a tuple with
-/// the aggregated result and the number of executions (=number of CPUs).
-/// 
-/// # Arguments
-/// 
-/// * `body` - closure that should be run in parallel on all available processors.
-/// * `initial` - initial value used for aggregating the results of all closure executions.
-/// * `agg` - closure used to aggregate method results.
-/// 
-/// # Examples
-/// 
-/// ```
-/// use mth_calc::run_on_all_cpus;
-/// use rand::prelude::*;
-/// 
-/// let result = mth_calc::run_on_all_cpus::<f32>(|| {
-///     // Create a thread-local random generator
-///     let mut rng = thread_rng();
-///
-///     // Calculate sum over ten random numbers
-///     let mut total = 0f32;
-///     for _ in 0..10 {
-///         total += rng.gen::<f32>();
-///     }
-///
-///     total
-/// }, 
-/// 0f32, // initial value for aggreation is zero
-/// |x, y| { x + y }); // aggregation function = sum
-/// 
-/// println!("The average random number was {}", result.0 / (result.1 * 10) as f32);
-/// ```
 pub fn run_on_all_cpus<T: Send + 'static>(body: fn() -> T, initial: T, agg: fn(T, T) -> T) -> (T, u32) {
     // Start one thread per CPU
     let num_cpus = num_cpus::get() as u32;

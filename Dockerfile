c292d76458b14d1424c2150074011d86f5787918
rstropek <rstropek@users.noreply.github.com>
2021-04-26 13:14:23 +0000

    deploy: dc4164bacf1fe5ffdde48fc03d300d6a11ad87b8

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM node
-COPY . /slides/
-WORKDIR /slides
-RUN npm install
-CMD ["npm", "start", "--", "--port", "80"]
--- util/clean.js
+++
@@ -1,6 +0,0 @@
-import fsPkg from 'fs-extra';
-const { rmdir } = fsPkg;
-
-(async () => {
-    await rmdir('dist', { recursive: true });
-})();
